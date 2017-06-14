7c8c23ac1101cb9d4c297f7b8b275c38fb074c2a
Bret Funk <bret.e.funk@gmail.com>
2017-06-14 16:06:17 -0500

    fixed bug giving nil a value, eliminated non-functioning methods

--- web_guesser.rb
+++ web_guesser.rb
@@ -12,18 +12,13 @@ get '/' do
   }
 end
 
-def filter(guess, number)
-  if guess.nil?
-  elsif guess == number
-    "You got it right!  The SECRET NUMBER is #{number}"
-  elsif guess > number
-    guess - 5 > number ? "Way too high! #{number}" : "Too high!  #{number}"
-  elsif guess < number
-    guess + 5 < number ? "Way too low! #{number}" : "Too low! #{number}"
-  end
-end
-
 def message(guess, number)
-  new_guess = guess.to_i unless nil
-  filter(new_guess, number)
+  if guess.nil?
+  elsif guess.to_i == number
+    "You got it right!  The SECRET NUMBER is #{number}"
+  elsif guess.to_i > number
+    guess.to_i - 5 > number ? "Way too high!" : "Too high!"
+  elsif guess.to_i < number
+    guess.to_i + 5 < number ? "Way too low!" : "Too low!"
+  end
 end
