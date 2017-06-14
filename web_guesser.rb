aeb206f3fb8ae01b34da475c009db52e812ae64d
Bret Funk <bret.e.funk@gmail.com>
2017-06-14 15:11:25 -0500

    working number message method and message param

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,6 +1,28 @@
 require 'sinatra'
 require 'sinatra/reloader'
+
 number = rand(100)
+
+
 get '/' do
-  erb :index, :locals => {:number => number}
+  erb :index, :locals => {
+    :number => number,
+    :user_guess => params["guess"],
+    :message => message(params["guess"], number)
+  }
+end
+
+def message(guess, number)
+  if guess.nil?
+  elsif guess.to_i > number + 5
+    "Way too high!"
+  elsif guess.to_i > number
+    "Too high!"
+  elsif guess.to_i < number + 5
+    "Way too low!"
+  elsif guess.to_i < number
+    "Too low!"
+  elsif guess.to_i == number
+    "You got it right!  \n The SECRET NUMBER is #{number}"
+  end
 end
